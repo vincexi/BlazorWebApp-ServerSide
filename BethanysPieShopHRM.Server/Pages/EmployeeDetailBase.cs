61609bebb427ebcf521b7619d66f4856d2a43e43
Vincent Duong <Vincent.Duong@fsresidential.com>
2020-05-22 00:28:06 -0400

    Add project files.

---
+++ BethanysPieShopHRM.Server/Pages/EmployeeDetailBase.cs
@@ -0,0 +1,31 @@
+﻿using BethanysPieShopHRM.ComponentsLibrary.Map;
+using BethanysPieShopHRM.Server.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.Server.Pages
+{
+    public class EmployeeDetailBase : ComponentBase
+    {
+        [Parameter]
+        public string EmployeeId { get; set; }
+        public Employee Employee { get; set; } = new Employee();
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
+        public List<Marker> MapMarkers { get; set; } = new List<Marker>();
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            MapMarkers = new List<Marker>
+            {
+                new Marker{Description = $"{Employee.FirstName} {Employee.LastName}",  ShowPopup = false, X = Employee.Longitude, Y = Employee.Latitude}
+            };
+        }
+
+    }
+}
---
+++ BethanysPieShopHRM.Server/Services/ICountryDataService.cs
@@ -0,0 +1,12 @@
+﻿using BethanysPieShopHRM.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.Server.Services
+{
+    public interface ICountryDataService
+    {
+        Task<IEnumerable<Country>> GetAllCountries();
+        Task<Country> GetCountryById(int countryId);
+    }
+}
